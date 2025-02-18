fba17b83789263a9e433a86b0de2304d8577b321
CristianCanoIng <canox09@gmail.com>
2025-02-18 12:02:04 -0500

    Nombre

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,20 +3,7 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("git config --global user, name");
-        System.out.println("git config --global user, email");
-        System.out.println("git init");
-        System.out.println("status");
-        System.out.println("git log");
-        System.out.println("git add .");
-        System.out.println("git commit");
-        System.out.println("git push");
-        System.out.println("git pull");
-        System.out.println("git switch");
-        System.out.println("git branch -D");
-        System.out.println("branch --delete");
-        System.out.println("Como devolver cambios de la rama");
-        System.out.println("git clon");
+        System.out.println("Daniel es marika");
 
     }
 }
