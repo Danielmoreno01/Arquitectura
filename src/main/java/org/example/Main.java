48a05a685e462c93940c7c06f6bf81dc2114a82a
CristianCanoIng <canox09@gmail.com>
2025-02-19 14:54:57 -0500

    revertir

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Daniel es marika");
-        System.out.println("Prueba de deshacer commit");
+        System.out.println("ejermplorever");
 
     }
 }
