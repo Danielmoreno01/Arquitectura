76b5cc4fb459a64d387dffeb4854cd59e4ee4ba4
CristianCanoIng <canox09@gmail.com>
2025-02-19 14:40:57 -0500

    pruebaborrar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Daniel es marika");
+        System.out.println("Prueba de deshacer commit");
 
     }
 }
